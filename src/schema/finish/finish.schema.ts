dc057f5230ac85336efef8c530378853c2d81a33
MarioMH8 <mhidalgomario@gmail.com>
2025-04-17 02:55:32 +0200

    feat: add glossy finish option to FinishSchema

--- src/schema/finish/finish.schema.ts
+++ src/schema/finish/finish.schema.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
 
-const FinishSchema = z.union([z.literal('nonFoil'), z.literal('foil'), z.literal('etched')]);
+const FinishSchema = z.union([z.literal('nonFoil'), z.literal('foil'), z.literal('etched'), z.literal('glossy')]);
 
 export default FinishSchema;
