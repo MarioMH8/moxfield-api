e62dd28c316df08f70dd8df2130c731b55b6aefc
MarioMH8 <mhidalgomario@gmail.com>
2025-05-29 17:23:54 +0200

    feat: add promos field to author schema

--- src/schema/author/author.schema.ts
+++ src/schema/author/author.schema.ts
@@ -5,6 +5,7 @@ const AuthorSchema = z
 		badges: z.array(z.string()),
 		displayName: z.string(),
 		profileImageUrl: z.string().url().nullish(),
+		promos: z.array(z.unknown()).nullish(),
 		userName: z.string(),
 	})
 	.strict();
