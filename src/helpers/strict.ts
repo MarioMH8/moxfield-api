d7f1bcf31f238f709be76bc36887b4d75e2f1b2a
MarioMH8 <mhidalgomario@gmail.com>
2025-07-10 17:22:43 +0200

    feat: enhance conditionalStrict function to support ZodObject and improve type safety

--- src/helpers/strict.ts
+++ src/helpers/strict.ts
@@ -1,13 +1,14 @@
+import type { ZodObject } from 'zod';
 import z from 'zod';
-import type { $ZodLooseShape } from 'zod/v4/core';
+import type { $ZodLooseShape, util } from 'zod/v4/core';
 
 import isTest from './is-test';
 
-export default function conditionalStrict<T extends $ZodLooseShape>(
-	schema: T
-): ReturnType<typeof z.strictObject<T>> | T {
+export default function conditionalStrict<Shape extends $ZodLooseShape, O extends ZodObject<util.Writeable<Shape>>>(
+	schema: O
+): O | ReturnType<typeof z.strictObject<Shape>> {
 	if (isTest()) {
-		return z.strictObject(schema);
+		return z.strictObject(schema.shape);
 	}
 
 	return schema;
